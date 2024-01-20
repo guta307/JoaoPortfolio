3094178757d21f8442d0f3b2617764cf657d2e76
guta307 <veiga187@gmail.com>
2024-01-20 14:07:55 -0300

    feat: Implement global.css with initial style settings
    This commit introduces the global.css file to the project, establishing foundational styling and typography. It includes the integration of custom fonts such as bessemer-bold, botanika-mono, rubik-italic, rubik-mediumitalic, planetkosmos, and coolvetica, enhancing the visual appeal and distinctiveness of the project's design. Additionally, the commit sets up basic CSS resets (zeroing margins, padding, and box-sizing) and defines a series of custom properties for screen sizes, font sizes, and a primary color theme in the Tailwind CSS framework. These initial configurations lay the groundwork for consistent and responsive design throughout the project.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+function toRgba(cssVariable: any) {
+  const color = `var(${cssVariable})`;
+  return ({ opacityValue }: any) => `rgba(${color}, ${opacityValue})`;
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      xs: "var(--text-xs)",
+      sm: "var(--text-sm)",
+      md: "var(--text-md)",
+      lg: "var(--text-lg)",
+      xl: "var(--text-xl)",
+      xxl: "var(--text-xxl)",
+      tmd: "var(--title-md)",
+      tlg: "var(--title-lg)",
+      txl: "var(--title-xl)",
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textColor: {
+        primary: toRgba("--primary"),
       },
     },
   },
   plugins: [],
 };
-export default config;
