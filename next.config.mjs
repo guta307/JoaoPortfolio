a9eb5101acaba4e333171d9055d20b0b841ab3b9
guta307 <veiga187@gmail.com>
2024-01-22 11:29:17 -0300

    feat: Configure Remote Image Patterns in Next.js
    Updated the Next.js configuration (`nextConfig`) to include a new remote pattern for handling images hosted on Google Drive. This allows images stored on Google Drive to be efficiently loaded and displayed within the application, leveraging Next.js's image optimization features. The hostname 'drive.google.com' has been specified under 'remotePatterns' to enable this functionality.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        // Se necessário, você pode adicionar configurações adicionais como porta, caminho, etc.
+      },
+    ],
+  },
+};
 
 export default nextConfig;
