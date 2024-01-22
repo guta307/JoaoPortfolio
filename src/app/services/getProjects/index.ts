8c0fc24daf9408a6b30a9392451354442da90116
guta307 <veiga187@gmail.com>
2024-01-22 11:24:31 -0300

    feat: Add Fetch Function for Project Data
    Implemented a new function `fetchDataFromBin` to request project data from JSONBin.io. The function takes a binId as an argument and makes a GET request using axios. It includes the API key in the request headers for authorization. The function handles

---
+++ src/app/services/getProjects/index.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+// Defina sua chave de API como uma variável de ambiente ou substitua-a diretamente aqui
+const API_KEY = process.env.NEXT_PUBLIC_MY_API_KEY;
+
+// Função para fazer uma solicitação GET para o bin
+async function fetchDataFromBin(binId: string): Promise<any> {
+  try {
+    const response = await axios.get(`https://api.jsonbin.io/v3/b/${binId}`, {
+      headers: {
+        "X-Master-Key": API_KEY,
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    // Verifique se a solicitação foi bem-sucedida
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Erro na solicitação GET");
+    }
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(`Erro ao obter dados do bin: ${error.message}`);
+  }
+}
+
+export default fetchDataFromBin;
