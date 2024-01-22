e91c3a8796524ce5465afc4f96f8409200702324
guta307 <veiga187@gmail.com>
2024-01-22 11:20:43 -0300

    feat: Update Home page with Headline component linking to project pages
    Modified the Home page to include Headline components that now link to individual project pages. Each Headline component displays a project with a specific media type and title, and is linked to its respective project page using unique IDs. This enhancement improves user navigation, allowing direct access to detailed project pages from the homepage. The integration of IDs in the URLs ensures a dynamic and efficient routing system, enhancing the overall user experience on the website.

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,6 +1,6 @@
 import Headline from "@/app/components/headline/headline";
 
-/*image imports*/
+// Importações de imagens
 import P1 from "../../assets/home/project1.png";
 import P2 from "../../assets/home/project2.png";
 import P3 from "../../assets/home/project3.png";
@@ -12,29 +12,48 @@ import P8 from "../../assets/home/project8.jpg";
 import P9 from "../../assets/home/project9.jpg";
 import P10 from "../../assets/home/project10.jpg";
 import P11 from "../../assets/home/project11.png";
+
+const ids = [
+  "65ad99b81f5677401f22a35e",
+  "65ad9d4f266cfc3fde7d922d",
+  "65ad9e621f5677401f22a4c6",
+  "65ad9fdc266cfc3fde7d9300",
+  "65ada040dc74654018981519",
+  "65ada0af1f5677401f22a583",
+  "65ada1091f5677401f22a5a6",
+  "65ad8b1e1f5677401f229eb8",
+  "65ada1a4266cfc3fde7d937d",
+  "65ada20a1f5677401f22a5fc",
+  "65ada2db1f5677401f22a629",
+];
+
 const home = () => {
   return (
     <div>
-      <div className=" flex tablet:gap-[0.5%] tablet:flex-col desktop:flex-row overflow-hidden mobile:flex-col  mobile:gap-0">
+      <div className="flex tablet:gap-[0.5%] desktop:flex-row overflow-hidden mobile:flex-col mobile:gap-0">
         <Headline
           url={P1}
           alt={"Documentário: A Arte por trás do mundo áspero"}
           media="documentário"
           title="A Arte por trás do mundo áspero"
+          link={`/pages/project/?id=${ids[0]}`}
         />
         <Headline
           url={P2}
           alt={"Documentário: Em terra de palmeiras: de 1914 a 1999"}
           media="documentário"
           title="Em terra de palmeiras: de 1914 a 1999"
+          link={`/pages/project/?id=${ids[1]}`}
         />
         <Headline
           url={P3}
           alt={"Documentário: MOCHAKK: HOUSE TO THE WORLD"}
           media="documentário"
           title="MOCHAKK: HOUSE TO THE WORLD"
+          link={`/pages/project/?id=${ids[2]}`}
         />
       </div>
+
       <div className="flex overflow-hidden">
         <Headline
           url={P4}
@@ -42,29 +61,34 @@ const home = () => {
           alt={"Documentário: O Sal da vida"}
           media="documentário"
           title="O sal da vida"
+          link={`/pages/project/?id=${ids[3]}`}
         />
       </div>
 
-      <div className=" flex tablet:gap-[0.5%] tablet:flex-col desktop:flex-row overflow-hidden mobile:flex-col  mobile:gap-0">
+      <div className="flex tablet:gap-[0.5%] desktop:flex-row overflow-hidden mobile:flex-col mobile:gap-0">
         <Headline
           url={P5}
           alt={"Ensaio: Akira e o retrato de uma geração"}
           media="ensaio"
           title="Akira o o retrato de uma geração"
+          link={`/pages/project/?id=${ids[4]}`}
         />
         <Headline
           url={P6}
           alt={"Ensaio: Entendendo wish you were here"}
           media="ensaio"
           title="Entendendo wish you were here"
+          link={`/pages/project/?id=${ids[5]}`}
         />
         <Headline
           url={P7}
           alt={"Ensaio: Tintin e a atemporalidade"}
           media="ensaio"
           title="Tintin e a atemporalidade"
+          link={`/pages/project/?id=${ids[6]}`}
         />
       </div>
+
       <div className="flex overflow-hidden">
         <Headline
           url={P8}
@@ -72,27 +96,31 @@ const home = () => {
           alt={"Ensaio: A vida e arte de Hayao Miazaki"}
           media="ensaio"
           title="A vida e arte de Hayao Miazaki"
+          link={`/pages/project/?id=${ids[7]}`}
         />
       </div>
 
-      <div className=" flex tablet:gap-[0.5%] tablet:flex-col desktop:flex-row overflow-hidden mobile:flex-col  mobile:gap-0">
+      <div className="flex tablet:gap-[0.5%] desktop:flex-row overflow-hidden mobile:flex-col mobile:gap-0">
         <Headline
           url={P9}
           alt={"Esquete: especial de hallowen"}
           media="esquete"
           title="Especial de hallowen"
+          link={`/pages/project/?id=${ids[8]}`}
         />
         <Headline
           url={P10}
           alt={"Institucional: A volta da maratona"}
           media="institucional"
           title="A volta da maratona"
+          link={`/pages/project/?id=${ids[9]}`}
         />
         <Headline
           url={P11}
           alt={"Esquete: the beartouille"}
           media="esquete"
           title="the beartouille"
+          link={`/pages/project/?id=${ids[10]}`}
         />
       </div>
     </div>
