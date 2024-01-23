f2056a7d9bfdf2d848f122a5af30c09c46f097a1
guta307 <veiga187@gmail.com>
2024-01-23 09:42:26 -0300

    feat(header): Enhance navbar with dynamic text color and z-index adjustments
    Improved the HeaderComponent to dynamically change the text color based on the current page. Removed the unused opacity mask from the navbar and corrected z-index values for better layering and visibility. This enhancement ensures a more intuitive and responsive navigation experience, highlighting the active page for better user orientation.

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,29 +1,45 @@
 import Link from "next/link";
+type headerProps = {
+  currentPage?: string;
+};
 
-const HeaderComponent = () => {
+const HeaderComponent = ({ currentPage }: headerProps) => {
   return (
-    <header className=" flex z-50 justify-between items-center fixed w-full pt-1 pb-1">
-      {/* Máscara para escurecer o header */}
-      <div
-        className="absolute w-screen h-full bg-black
-       opacity-100 -z-10"
-      ></div>
+    <nav className=" flex z-50 bg-black justify-between items-center fixed w-full pt-4 pb-4">
       <div>Logo</div>
 
       <ul className="flex gap-4 Header text-xl text-primary desktop:pr-8 tablet:pr-8 mobile:pr-4">
         <Link href="/pages/home">
-          <li className=" cursor-pointer">Home</li>
+          <li
+            className={`cursor-pointer ${
+              currentPage == "home" && " text-white"
+            }`}
+          >
+            Home
+          </li>
         </Link>
         <Link href="/pages/profile">
           {" "}
-          <li className=" cursor-pointer">Sobre</li>
+          <li
+            className={`cursor-pointer  ${
+              currentPage === "profile" && " text-white"
+            }`}
+          >
+            Sobre
+          </li>
         </Link>
         <Link href="/pages/contact">
           {" "}
-          <li className=" cursor-pointer">Contato</li>
+          <li
+            className={`cursor-pointer  ${
+              currentPage === "contact" && " text-white"
+            }`}
+          >
+            Contato
+          </li>
         </Link>
       </ul>
-    </header>
+    </nav>
   );
 };
 
