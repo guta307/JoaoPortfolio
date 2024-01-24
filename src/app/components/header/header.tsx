943b5ce3a70607570b3913003d3985aa0d6a2106
guta307 <veiga187@gmail.com>
2024-01-24 09:05:43 -0300

    (feat) - added logo to the header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
+
+import Logo from "../../assets/header/Logo.png";
+import Image from "next/image";
 type headerProps = {
   currentPage?: string;
 };
 
 const HeaderComponent = ({ currentPage }: headerProps) => {
   return (
-    <nav className=" flex z-50 bg-black justify-between items-center fixed w-full pt-4 pb-4">
-      <div>Logo</div>
+    <nav className=" my-0 mx-auto flex z-50 bg-black justify-between items-center fixed w-full min-w-[300px] pt-4 pb-4">
+      <Image
+        src={Logo}
+        alt="imagem de logo de João Pastoriza"
+        className=" desktop:pl-8 tablet:pl-8 mobile:pl-4 desktop:w-[20%] mobile:w-[25%]"
+      />
 
       <ul className="flex gap-4 Header text-xl text-primary desktop:pr-8 tablet:pr-8 mobile:pr-4">
         <Link href="/pages/home">
