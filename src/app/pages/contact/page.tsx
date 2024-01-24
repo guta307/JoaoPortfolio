98269c8d5e23cdf3b32c49c59be18d547963767b
guta307 <veiga187@gmail.com>
2024-01-24 13:18:36 -0300

    fix(style) - resolution of spacing problems between the contact and the footer, that now always stays at the bottom

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -3,39 +3,41 @@ import HeaderComponent from "@/app/components/header/header";
 
 const Contact = () => {
   return (
-    <div className="h-screen flex flex-col justify-between">
+    <>
       <HeaderComponent currentPage="contact" />
-      <div className="  flex gap-8 pt-[6rem]  flex-col text-center  items-center">
-        <section className={"text-primary"}>
-          <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
-            Endereço
-          </h1>
-          <p className="article text-xxl mobile:text-lg">
-            Vila Mariana, São Paulo
-          </p>
-        </section>
+      <div className=" h-[90%] flex flex-col justify-between">
+        <div className="  flex gap-8 pt-[6rem] flex-col text-center  items-center">
+          <section className={"text-primary"}>
+            <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
+              Endereço
+            </h1>
+            <p className="article text-xxl mobile:text-lg">
+              Vila Mariana, São Paulo
+            </p>
+          </section>
 
-        <section className={"text-primary"}>
-          <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
-            E-mail
-          </h1>
-          <p className="article desktop:text-xxl mobile:text-lg">
-            joaovpastoriza@gmail.com
-          </p>
-        </section>
+          <section className={"text-primary"}>
+            <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
+              E-mail
+            </h1>
+            <p className="article desktop:text-xxl mobile:text-lg">
+              joaovpastoriza@gmail.com
+            </p>
+          </section>
 
-        <section className={"text-primary"}>
-          <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
-            Telefone
-          </h1>
-          <p className="article text-xxl desktop:text-xxl mobile:text-lg">
-            (15) 99813-8404
-          </p>
-        </section>
+          <section className={"text-primary"}>
+            <h1 className="Header font-bold desktop:text-tlg mobile:text-tmd">
+              Telefone
+            </h1>
+            <p className="article text-xxl desktop:text-xxl mobile:text-lg">
+              (15) 99813-8404
+            </p>
+          </section>
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
